4a256f33f627bf9f532c21c50346c52e1733ecb6
keshu <keshuvodedara@gmail.com>
2019-01-24 16:11:28 +0530

    download book

--- app/src/main/java/app/biblion/activity/SplashActivity.java
+++ app/src/main/java/app/biblion/activity/SplashActivity.java
@@ -1,9 +1,16 @@
 package app.biblion.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import app.biblion.R;
 import app.biblion.sessionmanager.SessionManager;
@@ -13,16 +20,22 @@ public class SplashActivity extends AppCompatActivity {
     private final int SPLASH_TIME = 100;
     SessionManager securityManager;
     Intent intent;
+
+    public static final int MULTIPLE_PERMISSIONS = 10;
+    String[] permissions = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         securityManager = new SessionManager(SplashActivity.this);
-        if(securityManager.checkLogin()){
-            intent=new Intent(SplashActivity.this, LoginActivity.class);
+        if (securityManager.checkLogin()) {
+            intent = new Intent(SplashActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
-        }else {
-            intent=new Intent(SplashActivity.this, NavigationActivity.class);
+        } else {
+            intent = new Intent(SplashActivity.this, NavigationActivity.class);
             startActivity(intent);
             finish();
         }
@@ -34,6 +47,7 @@ public class SplashActivity extends AppCompatActivity {
 
                     }
                 }, SPLASH_TIME);
+        call_permissions();
       /*  addSlide(AppIntroFragment.newInstance("STEP 1", "Tap to open instagram.", R.drawable.instaintro1, Color.parseColor("#e91e63")));
         addSlide(AppIntroFragment.newInstance("STEP 2", "Tap on more option icon over instagram post.", R.drawable.instaintro2, Color.parseColor("#4caf50")));
         addSlide(AppIntroFragment.newInstance("STEP 3", "Tap on 'Copy Share URL' option. NOTE : If users profile is private 'Copy share URL' option is not available", R.drawable.instaintro3, Color.parseColor("#00bcd4")));
@@ -41,6 +55,22 @@ public class SplashActivity extends AppCompatActivity {
         showSkipButton(false);*/
     }
 
+
+    private void call_permissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(getApplicationContext(), p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray
+                    (new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+        }
+        return;
+    }
   /*  @Override
     public void onSkipPressed(Fragment currentFragment) {
 
--- app/src/main/java/app/biblion/fragment/DetailELibraryFragment.java
+++ app/src/main/java/app/biblion/fragment/DetailELibraryFragment.java
@@ -1,23 +1,36 @@
 package app.biblion.fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.devs.readmoreoption.ReadMoreOption;
 
+import java.io.File;
+
 import app.biblion.R;
+import app.biblion.notifacation.BackgroundNotificationService;
 
 public class DetailELibraryFragment extends Fragment {
 
     View mView;
-
+    private static final String TAG = "DetailELibraryFragment";
     RatingBar bookRatingBar;
-    TextView txtDescription;
+    TextView txtDescription,btnBookDownload;
+    public static final String PROGRESS_UPDATE = "progress_update";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,6 +42,7 @@ public class DetailELibraryFragment extends Fragment {
     }
 
     private void initialization() {
+        btnBookDownload=mView.findViewById(R.id.btnBookDownload);
         txtDescription = mView.findViewById(R.id.txtDescription);
         bookRatingBar = mView.findViewById(R.id.book_rating);
         bookRatingBar.setRating((float) 2.5);
@@ -42,7 +56,53 @@ public class DetailELibraryFragment extends Fragment {
                 .build();
 
         readMoreOption.addReadMoreTo(txtDescription, getString(R.string.long_desc));
+        registerReceiver();
+        btnBookDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startImageDownload();
+            }
+        });
     }
 
 
+
+    private void registerReceiver() {
+
+        LocalBroadcastManager bManager = LocalBroadcastManager.getInstance(getActivity());
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(PROGRESS_UPDATE);
+        bManager.registerReceiver(mBroadcastReceiver, intentFilter);
+
+    }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (intent.getAction().equals(PROGRESS_UPDATE)) {
+
+                boolean downloadComplete = intent.getBooleanExtra("downloadComplete", false);
+                //Log.d("API123", download.getProgress() + " current progress");
+
+                if (downloadComplete) {
+
+                    Toast.makeText(getActivity(), "File download completed", Toast.LENGTH_SHORT).show();
+
+                    File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + File.separator +
+                            "Book.EPUB");
+                    Log.e(TAG, "onReceive: " + file);
+
+                }
+            }
+        }
+    };
+
+    private void startImageDownload() {
+
+
+        Intent intent = new Intent(getActivity(), BackgroundNotificationService.class);
+        getActivity().startService(intent);
+
+    }
 }
