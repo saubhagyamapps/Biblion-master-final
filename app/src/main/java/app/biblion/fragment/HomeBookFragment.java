86c44ed0d50a27cbac3aa5870f111caf9e61da86
keshu <keshuvodedara@gmail.com>
2019-02-18 19:38:13 +0530

    keshu

--- app/src/main/java/app/biblion/fragment/HomeBookFragment.java
+++ app/src/main/java/app/biblion/fragment/HomeBookFragment.java
@@ -18,7 +18,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
-import android.text.Layout;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.UnderlineSpan;
@@ -27,12 +26,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.bluejamesbond.text.DocumentView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-
 import com.viewpagerindicator.CirclePageIndicator;
 
 import java.io.File;
@@ -52,14 +50,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
-
 
 public class HomeBookFragment extends Fragment {
     private static final String TAG = "HomeBookFragment";
     View mView;
     Context context;
-    ImageView devotion_image;
+    ImageView devotion_image, ssImagesView;
     TextView txt_BhaktiDesc, txtDevotion, titleDevotion;
     private boolean isLoading = false;
     private boolean isLastPage = false;
@@ -74,12 +70,13 @@ public class HomeBookFragment extends Fragment {
     Uri uri;
     Bitmap bitmap;
     Spanned dicription;
-
+    LinearLayout layoutScreenShort;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    CardView Card_Bible, Card_Quiz, Card_SongBook, Card_Article,Card_E_Library;
+    CardView Card_Bible, Card_Quiz, Card_SongBook, Card_Article, Card_E_Library;
     JustifyTextView justifyTextView;
-
+    TextView txtImagesTitel;
+    TextView txtAppLink;
 
     @Nullable
     @Override
@@ -94,9 +91,13 @@ public class HomeBookFragment extends Fragment {
 
         viewPager = mView.findViewById(R.id.pager);
         devotion_image = mView.findViewById(R.id.image_devotion);
-       // txt_BhaktiDesc = mView.findViewById(R.id.bhakti_txt);
+        // txt_BhaktiDesc = mView.findViewById(R.id.bhakti_txt);
         titleDevotion = mView.findViewById(R.id.devotion_title);
         justifyTextView = mView.findViewById(R.id.bhakti_txt);
+        layoutScreenShort = mView.findViewById(R.id.layoutScreenShort);
+        txtImagesTitel = mView.findViewById(R.id.txtImagesTitel);
+        ssImagesView = mView.findViewById(R.id.ssImagesView);
+
 
         Card_Bible = mView.findViewById(R.id.home_card_bible);
         Card_Quiz = mView.findViewById(R.id.home_card_quiz);
@@ -106,7 +107,6 @@ public class HomeBookFragment extends Fragment {
         clicked();
         fabShare = mView.findViewById(R.id.fabShare);
         txtDevotion = mView.findViewById(R.id.txtDevotion);
-        txt_BhaktiDesc.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
         getImagedata();
         LoadBhaktidata();
         fabShare.setOnClickListener(new View.OnClickListener() {
@@ -120,8 +120,7 @@ public class HomeBookFragment extends Fragment {
         });
     }
 
-    private void clicked()
-    {
+    private void clicked() {
         Card_Bible.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -129,7 +128,7 @@ public class HomeBookFragment extends Fragment {
                 BibleBookFragment bibleBookFragment = new BibleBookFragment();
                 getFragmentManager().beginTransaction()
                         .addToBackStack(null)
-                        .replace(R.id.contant_frame,bibleBookFragment)
+                        .replace(R.id.contant_frame, bibleBookFragment)
                         .commit();
             }
         });
@@ -139,7 +138,7 @@ public class HomeBookFragment extends Fragment {
                 QuizFragment quizFragment = new QuizFragment();
                 getFragmentManager().beginTransaction()
                         .addToBackStack(null)
-                        .replace(R.id.contant_frame,quizFragment)
+                        .replace(R.id.contant_frame, quizFragment)
                         .commit();
             }
         });
@@ -147,10 +146,10 @@ public class HomeBookFragment extends Fragment {
         Card_SongBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SongBookFragment songBookFragment =new SongBookFragment();
+                SongBookFragment songBookFragment = new SongBookFragment();
                 getFragmentManager().beginTransaction()
                         .addToBackStack(null)
-                        .replace(R.id.contant_frame,songBookFragment)
+                        .replace(R.id.contant_frame, songBookFragment)
                         .commit();
             }
         });
@@ -161,7 +160,7 @@ public class HomeBookFragment extends Fragment {
                 ArticlesFragment articlesFragment = new ArticlesFragment();
                 getFragmentManager().beginTransaction()
                         .addToBackStack(null)
-                        .replace(R.id.contant_frame,articlesFragment)
+                        .replace(R.id.contant_frame, articlesFragment)
                         .commit();
             }
         });
@@ -171,7 +170,7 @@ public class HomeBookFragment extends Fragment {
                 ELibraryFragment eLibraryFragment = new ELibraryFragment();
                 getFragmentManager().beginTransaction()
                         .addToBackStack(null)
-                        .replace(R.id.contant_frame,eLibraryFragment)
+                        .replace(R.id.contant_frame, eLibraryFragment)
                         .commit();
             }
         });
@@ -187,7 +186,7 @@ public class HomeBookFragment extends Fragment {
             String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
 
             // create bitmap screen capture
-            View v1 = getActivity().getWindow().getDecorView().findViewById(R.id.image_devotion);
+            View v1 = getActivity().getWindow().getDecorView().findViewById(R.id.layoutScreenShort);
             v1.setDrawingCacheEnabled(true);
             Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
             v1.setDrawingCacheEnabled(false);
@@ -209,10 +208,10 @@ public class HomeBookFragment extends Fragment {
     private void openScreenshot(File imageFile) {
 
         Uri uri = Uri.fromFile(imageFile);
-        String textToShare = "Keshu odedara";
+
         Intent share = new Intent(Intent.ACTION_SEND);
         share.setType("image/*");
-        share.putExtra(Intent.EXTRA_TEXT, String.valueOf(dicription));
+        share.putExtra(Intent.EXTRA_TEXT, String.valueOf(dicription)+"https://play.google.com/store/apps/dev?id=7665705150185283127");
         share.putExtra(Intent.EXTRA_STREAM, uri);
         startActivity(Intent.createChooser(share, "Share with"));
     }
@@ -303,7 +302,7 @@ public class HomeBookFragment extends Fragment {
 
 
                 String txtDesc = response.body().getDescription().replace("&lt;", "<").replace("&gt;", ">")
-                        .replace("\\r\\n\\r\\n", "").replace("\\r\\n","");
+                        .replace("\\r\\n\\r\\n", "").replace("\\r\\n", "");
 
                 Glide.with(getActivity()).load(response.body().getImage())
                         .thumbnail(0.5f)
@@ -311,10 +310,16 @@ public class HomeBookFragment extends Fragment {
                         .skipMemoryCache(true)
                         .diskCacheStrategy(DiskCacheStrategy.ALL)
                         .into(devotion_image);
+                Glide.with(getActivity()).load(response.body().getImage())
+                        .thumbnail(0.5f)
+                        .crossFade()
+                        .skipMemoryCache(true)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .into(ssImagesView);
                 String strinHtml = txtDesc.replace("&amp;nbsp;", "  ");
                 dicription = (Html.fromHtml(strinHtml));
                 //txt_BhaktiDesc.setText(dicription);
-               // SpannableString spannableString = new SpannableString(response.body().getDescription());
+                // SpannableString spannableString = new SpannableString(response.body().getDescription());
                 //description (Html.fromHtml(strinHtml));
                 justifyTextView.setText(String.valueOf(dicription));
 
@@ -322,7 +327,7 @@ public class HomeBookFragment extends Fragment {
                 SpannableString content = new SpannableString(response.body().getTitle());
                 content.setSpan(new UnderlineSpan(), 0, response.body().getTitle().length(), 0);
                 titleDevotion.setText(content);
-
+                txtImagesTitel.setText(response.body().getTitle());
 
 
             }
@@ -336,92 +341,4 @@ public class HomeBookFragment extends Fragment {
 
 
     }
-        /*recyclerView_Home = mView.findViewById(R.id.recyclerview_home);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView_Home.setLayoutManager(layoutManager);
-        homeViewPagerAdapter = new HomeViewPagerAdapter(getActivity(),dataBeanlist);
-        recyclerView_Home.setAdapter(homeViewPagerAdapter);
-
-            for (int i = 0; i<dataBeanlist.size(); i++)
-            {
-                dataBeanlist.add
-            }*/
-       /* recyclerView_Home.addOnScrollListener(new PaginationScrollListenerLinear((LinearLayoutManager) layoutManager) {
-            @Override
-            protected void loadMoreItems() {
-                isLoading = true;
-                currentPage += 1;
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        loadNextPage();
-                    }
-                }, 1000);
-            }
-
-            @Override
-            public int getTotalPageCount() {
-                return TOTAL_PAGES;
-            }
-
-            @Override
-            public boolean isLastPage() {
-                return isLastPage;
-            }
-
-            @Override
-            public boolean isLoading() {
-                return isLoading;
-            }
-        });
-        loadFirstPage();
-    }*/
-
-       /* private void loadFirstPage () {
-            Log.d(TAG, "loadFirstPage:");
-            Call<HomeModel> modelCall = Constant.apiService.getHomeList(currentPage);
-            modelCall.enqueue(new Callback<HomeModel>() {
-                @Override
-                public void onResponse(Call<HomeModel> call, Response<HomeModel> response) {
-                    Constant.mImagesPath = response.body().getPath();
-                    List<HomeModel.ResultBean> resultBeans = response.body().getResult();
-
-                    homeAdapter.addAll(resultBeans);
-                    if (currentPage <= TOTAL_PAGES) homeAdapter.addLoadingFooter();
-                    else isLastPage = true;
-                }
-
-                @Override
-                public void onFailure(Call<HomeModel> call, Throwable t) {
-
-                }
-            });
-        }
-
-        private void loadNextPage () {
-            Log.d(TAG, "loadNextPage:" + currentPage);
-            Call<HomeModel> modelCall = Constant.apiService.getHomeList(currentPage);
-            modelCall.enqueue(new Callback<HomeModel>() {
-                @Override
-                public void onResponse(Call<HomeModel> call, Response<HomeModel> response) {
-
-                    homeAdapter.removeLoadingFooter();
-                    isLoading = false;
-
-                    List<HomeModel.ResultBean> resultBeans = response.body().getResult();
-
-                    homeAdapter.addAll(resultBeans);
-
-                    if (currentPage != TOTAL_PAGES) homeAdapter.addLoadingFooter();
-                    else isLastPage = true;
-                }
-
-                @Override
-                public void onFailure(Call<HomeModel> call, Throwable t) {
-
-                }
-            });
-        }*/
-
 }
